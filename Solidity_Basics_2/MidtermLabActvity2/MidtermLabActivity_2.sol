c2702a19b1e46c2346267d19ae885ba4397a7c03
jasleorez <123354200+jasleorez@users.noreply.github.com>
2024-02-28 02:24:11 +0800

    Add files via upload

---
+++ Solidity_Basics_2/MidtermLabActvity2/MidtermLabActivity_2.sol
@@ -0,0 +1,30 @@
+pragma solidity ^0.8.0;
+
+contract Ownable {
+    address public owner;
+
+    event OwnershipTransferred(address indexed previousOwner, address indexed newOwner);
+
+    constructor() {
+        owner = msg.sender;
+    }
+
+    modifier onlyOwner() {
+        require(msg.sender == owner, "Only the owner can call this function");
+        _;
+    }
+
+    function transferOwnership(address newOwner) public onlyOwner {
+        require(newOwner != address(0), "Invalid new owner address");
+        emit OwnershipTransferred(owner, newOwner);
+        owner = newOwner;
+    }
+}
+
+contract MyContract is Ownable {
+    uint256 public someValue;
+
+    function setSomeValue(uint256 newValue) public onlyOwner {
+        someValue = newValue;
+    }
+}
